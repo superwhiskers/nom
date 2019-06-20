57b6ff966508c4f1526b6d6dbf7641142c54b573
superwhiskers <whiskerdev@protonmail.com>
2019-06-20 00:47:49 -0500

    rename the minibuffer to be the buffer because crunch's buffer will never be ported over as that would make no sense

---
+++ nom.c
@@ -0,0 +1,282 @@
+/*
+
+nom - crunch but the letter c
+copyright (c) 2019 superwhiskers <whiskerdev@protonmail.com>
+
+this program is free software: you can redistribute it and/or modify
+it under the terms of the gnu lesser general public license as published by
+the free software foundation, either version 3 of the license, or
+(at your option) any later version.
+
+this program is distributed in the hope that it will be useful,
+but without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.  see the
+gnu lesser general public license for more details.
+
+you should have received a copy of the gnu lesser general public license
+along with this program.  if not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+#include "nom.h"
+
+/* creation and destruction functions */
+
+void nom_buffer_new(struct nom_buffer *out, int initial_size) {
+
+	out->off = 0x00;
+	out->cap = initial_size;
+	out->boff = 0x00;
+	out->bcap = initial_size*8;
+
+	out->buf = malloc(initial_size*sizeof(char));
+
+}
+
+void nom_buffer_destroy(struct nom_buffer *b) {
+
+	free(b->buf);
+	free(b);
+
+}
+
+/* bitfield functions */
+
+void nom_buffer_readbit(struct nom_buffer *b, char *out, int off) {
+
+	*out = (b->buf[off/8] >> (7 - (off%8))) & 1;
+
+}
+
+void nom_buffer_readbitnext(struct nom_buffer *b, char *out) {
+
+	nom_buffer_readbit(b, out, b->boff);
+	nom_buffer_seekbit(b, 1, 1);
+
+}
+
+void nom_buffer_readbits(struct nom_buffer *b, int *out, int off, int n) {
+
+	char bout;
+	for (int i = 0; i < n; i++) {
+
+		nom_buffer_readbit(b, &bout, off+i);
+		*out = (*out << 1) | bout;
+
+	}
+
+}
+
+void nom_buffer_readbitsnext(struct nom_buffer *b, int *out, int n) {
+
+	nom_buffer_readbits(b, out, b->boff, n);
+	nom_buffer_seekbit(b, n, 1);
+	
+}
+
+void nom_buffer_setbit(struct nom_buffer *b, int off) {
+
+	b->buf[off/8] |= (1 << (7-(off%8)));
+
+}
+
+void nom_buffer_setbitnext(struct nom_buffer *b) {
+
+	nom_buffer_setbit(b, b->boff);
+	nom_buffer_seekbyte(b, 1, 1);
+
+}
+
+void nom_buffer_clearbit(struct nom_buffer *b, int off) {
+
+	b->buf[off/8] &= ~(1 << (7-(off%8)));
+
+}
+
+void nom_buffer_clearbitnext(struct nom_buffer *b) {
+
+	nom_buffer_clearbit(b, b->boff);
+	nom_buffer_seekbyte(b, 1, 1);
+
+}
+
+void nom_buffer_setbits(struct nom_buffer *b, int off, int data, int n) {
+
+	for (int i = 0; i < n; i++) {
+
+		if (((data>>(n-i-1))&1) == 0) {
+
+			nom_buffer_clearbit(b, off + i);
+
+		} else {
+
+			nom_buffer_setbit(b, off + i);
+
+		}
+
+	}
+
+}
+
+void nom_buffer_setbitsnext(struct nom_buffer *b, int data, int n) {
+
+	nom_buffer_setbits(b, b->boff, data, n);
+	nom_buffer_seekbyte(b, n, 1);
+
+}
+
+void nom_buffer_flipbit(struct nom_buffer *b, int off) {
+
+	b->buf[off/8] ^= (1 << (7-(off%8)));
+
+}
+
+void nom_buffer_flipbitnext(struct nom_buffer *b) {
+
+	nom_buffer_flipbit(b, b->boff);
+	nom_buffer_seekbit(b, 1, 1);
+
+}
+
+void nom_buffer_clearallbits(struct nom_buffer *b) {
+
+	for (int i = 0; i < b->cap; i++) {
+
+		b->buf[i] = 0x00;
+		
+	}
+
+}
+
+void nom_buffer_setallbits(struct nom_buffer *b) {
+
+	for (int i = 0; i < b->cap; i++) {
+
+		b->buf[i] = 0xff;
+
+	}
+
+}
+
+void nom_buffer_flipallbits(struct nom_buffer *b) {
+
+	for (int i = 0; i < b->cap; i++) {
+
+		b->buf[i] = ~b->buf[i];
+
+	}
+
+}
+
+void nom_buffer_seekbit(struct nom_buffer *b, int off, int relative) {
+
+	if (relative < 0) {
+
+		b->boff = off;
+
+	} else if (relative >= 0) {
+
+		b->boff += off;
+
+	}
+
+}
+
+void nom_buffer_afterbit(struct nom_buffer *b, int *out, int off) {
+
+	if (off < 0) {
+
+		*out = b->bcap - b->boff - 1;
+
+	} else if (off >= 0) {
+
+		*out = b->bcap - off - 1;
+
+	}
+
+}
+
+void nom_buffer_alignbit(struct nom_buffer *b) {
+
+	b->boff = b->off*8;
+
+}
+
+/* bytebuffer functions */
+
+void nom_buffer_writebytes(struct nom_buffer *b, int off, int data_length, char *data) {
+
+	memcpy(b->buf+off, data, data_length*sizeof(char)); 
+	
+}
+
+void nom_buffer_writebytesnext(struct nom_buffer *b, int n, char *data) {
+
+	nom_buffer_writebytes(b, b->off, n, data);
+	nom_buffer_seekbyte(b, n, 1);
+
+}
+
+void nom_buffer_readbytes(struct nom_buffer *b, char *out, int off, int n) {
+
+	memcpy(out, b->buf+off, n*sizeof(char));
+
+}
+
+void nom_buffer_readbytesnext(struct nom_buffer *b, char *out, int n) {
+
+	nom_buffer_readbytes(b, out, b->off, n);
+	nom_buffer_seekbyte(b, n, 1);
+
+}
+
+void nom_buffer_seekbyte(struct nom_buffer *b, int off, int relative) {
+
+	if (relative < 0) {
+
+		b->off = off;
+
+	} else if (relative >= 0) {
+
+		b->off += off;
+
+	}
+
+}
+
+void nom_buffer_afterbyte(struct nom_buffer *b, int *out, int off) {
+
+	if (off < 0) {
+
+		*out = b->cap - b->off - 1;
+
+	} else if (off >= 0) {
+
+		*out = b->cap - off - 1;
+
+	}
+
+}
+
+void nom_buffer_alignbyte(struct nom_buffer *b) {
+
+	b->off = b->boff/8;
+
+}
+
+/* generic functions */
+
+void nom_buffer_grow(struct nom_buffer *b, int n) {
+
+	char *old = b->buf;
+
+	b->buf = malloc(sizeof(*old)+n);
+	memcpy(b->buf, old, sizeof(*old));
+
+	b->cap = b->cap + n;
+	b->bcap = b->cap*8;
+
+	free(old);
+
+}
+
--- readme.md
+++ readme.md
@@ -28,31 +28,31 @@ void print_char_array(int size, char *arr) {
 int main() {
 
 	// create a container for the buffer
-	struct nom_minibuffer *buf = malloc(sizeof(struct nom_minibuffer));
+	struct nom_buffer *buf = malloc(sizeof(struct nom_buffer));
 
 	// creates a new buffer with four zeroes
-	nom_minibuffer_new(buf, 4);
+	nom_buffer_new(buf, 4);
 
 	// write the byte `0x01` to the first offset, and move the offset forward one
-	nom_minibuffer_writebytesnext(buf, 1, (char[]){0x01});
+	nom_buffer_writebytesnext(buf, 1, (char[]){0x01});
 
 	// write the byte `0x01` to the second offset, and move the offset forward one
-	nom_minibuffer_writebytesnext(buf, 1, (char[]){0x01});
+	nom_buffer_writebytesnext(buf, 1, (char[]){0x01});
 
 	// seek the offset back one
-	nom_minibuffer_seekbyte(buf, -1, 1);
+	nom_buffer_seekbyte(buf, -1, 1);
 	
 	// write the bytes `0x02` and `0x03` to the second and third offsets, respectively
-	nom_minibuffer_writebytesnext(buf, 2, (char[]){0x02, 0x03});
+	nom_buffer_writebytesnext(buf, 2, (char[]){0x02, 0x03});
 
 	// write the byte `0x04` to offset `0x03`
-	nom_minibuffer_writebytes(buf, 0x03, 1, (char[]){0x04});
+	nom_buffer_writebytes(buf, 0x03, 1, (char[]){0x04});
 
 	// output the buffer's contents to the console
 	print_char_array(buf->cap, buf->buf);
 
 	// free the used memory
-	nom_minibuffer_destroy(buf);
+	nom_buffer_destroy(buf);
 
   return 0;
 
