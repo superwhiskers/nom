1039b0edede9b8dd80e87e713563facbb5e2a92d
superwhiskers <whiskerdev@protonmail.com>
2019-07-17 13:14:20 -0500

    add rcomplex & rename `nom_buffer` -> `NomBuffer` and add a typedef for it, while still allowing forward declaration

--- nom.c
+++ nom.c
@@ -25,7 +25,7 @@ along with this program.  if not, see <https://www.gnu.org/licenses/>.
 
 /* creation and destruction functions */
 
-void nom_buffer_new(struct nom_buffer *out, long long initial_size) {
+void nom_buffer_new(struct NomBuffer *out, long long initial_size) {
 	out->off = 0x00;
 	out->cap = initial_size;
 	out->boff = 0x00;
@@ -34,24 +34,24 @@ void nom_buffer_new(struct nom_buffer *out, long long initial_size) {
 	out->buf = malloc(initial_size * sizeof(unsigned char));
 }
 
-void nom_buffer_destroy(struct nom_buffer *b) {
+void nom_buffer_destroy(struct NomBuffer *b) {
 	free(b->buf);
 	free(b);
 }
 
 /* bitfield functions */
 
-void nom_buffer_readbit(struct nom_buffer *b, unsigned char *out,
+void nom_buffer_readbit(struct NomBuffer *b, unsigned char *out,
 			long long off) {
 	*out = (b->buf[off / 8] >> (7 - (off % 8))) & 1;
 }
 
-void nom_buffer_readbitnext(struct nom_buffer *b, unsigned char *out) {
+void nom_buffer_readbitnext(struct NomBuffer *b, unsigned char *out) {
 	nom_buffer_readbit(b, out, b->boff);
 	nom_buffer_seekbit(b, 1, 1);
 }
 
-void nom_buffer_readbits(struct nom_buffer *b, unsigned long long *out,
+void nom_buffer_readbits(struct NomBuffer *b, unsigned long long *out,
 			 long long off, long long n) {
 	unsigned char bout;
 	for (int i = 0; i < n; i++) {
@@ -60,31 +60,31 @@ void nom_buffer_readbits(struct nom_buffer *b, unsigned long long *out,
 	}
 }
 
-void nom_buffer_readbitsnext(struct nom_buffer *b, unsigned long long *out,
+void nom_buffer_readbitsnext(struct NomBuffer *b, unsigned long long *out,
 			     long long n) {
 	nom_buffer_readbits(b, out, b->boff, n);
 	nom_buffer_seekbit(b, n, 1);
 }
 
-void nom_buffer_setbit(struct nom_buffer *b, long long off) {
+void nom_buffer_setbit(struct NomBuffer *b, long long off) {
 	b->buf[off / 8] |= (1 << (7 - (off % 8)));
 }
 
-void nom_buffer_setbitnext(struct nom_buffer *b) {
+void nom_buffer_setbitnext(struct NomBuffer *b) {
 	nom_buffer_setbit(b, b->boff);
 	nom_buffer_seekbyte(b, 1, 1);
 }
 
-void nom_buffer_clearbit(struct nom_buffer *b, long long off) {
+void nom_buffer_clearbit(struct NomBuffer *b, long long off) {
 	b->buf[off / 8] &= ~(1 << (7 - (off % 8)));
 }
 
-void nom_buffer_clearbitnext(struct nom_buffer *b) {
+void nom_buffer_clearbitnext(struct NomBuffer *b) {
 	nom_buffer_clearbit(b, b->boff);
 	nom_buffer_seekbyte(b, 1, 1);
 }
 
-void nom_buffer_setbits(struct nom_buffer *b, long long off,
+void nom_buffer_setbits(struct NomBuffer *b, long long off,
 			unsigned long long data, long long n) {
 	for (int i = 0; i < n; i++) {
 		if (((data >> (n - i - 1)) & 1) == 0) {
@@ -96,40 +96,40 @@ void nom_buffer_setbits(struct nom_buffer *b, long long off,
 	}
 }
 
-void nom_buffer_setbitsnext(struct nom_buffer *b, unsigned long long data,
+void nom_buffer_setbitsnext(struct NomBuffer *b, unsigned long long data,
 			    long long n) {
 	nom_buffer_setbits(b, b->boff, data, n);
 	nom_buffer_seekbyte(b, n, 1);
 }
 
-void nom_buffer_flipbit(struct nom_buffer *b, long long off) {
+void nom_buffer_flipbit(struct NomBuffer *b, long long off) {
 	b->buf[off / 8] ^= (1 << (7 - (off % 8)));
 }
 
-void nom_buffer_flipbitnext(struct nom_buffer *b) {
+void nom_buffer_flipbitnext(struct NomBuffer *b) {
 	nom_buffer_flipbit(b, b->boff);
 	nom_buffer_seekbit(b, 1, 1);
 }
 
-void nom_buffer_clearallbits(struct nom_buffer *b) {
+void nom_buffer_clearallbits(struct NomBuffer *b) {
 	for (int i = 0; i < b->cap; i++) {
 		b->buf[i] = 0x00;
 	}
 }
 
-void nom_buffer_setallbits(struct nom_buffer *b) {
+void nom_buffer_setallbits(struct NomBuffer *b) {
 	for (int i = 0; i < b->cap; i++) {
 		b->buf[i] = 0xff;
 	}
 }
 
-void nom_buffer_flipallbits(struct nom_buffer *b) {
+void nom_buffer_flipallbits(struct NomBuffer *b) {
 	for (int i = 0; i < b->cap; i++) {
 		b->buf[i] = ~b->buf[i];
 	}
 }
 
-void nom_buffer_seekbit(struct nom_buffer *b, long long off,
+void nom_buffer_seekbit(struct NomBuffer *b, long long off,
 			unsigned char relative) {
 	if (relative < 0) {
 		b->boff = off;
@@ -139,7 +139,7 @@ void nom_buffer_seekbit(struct nom_buffer *b, long long off,
 	}
 }
 
-void nom_buffer_afterbit(struct nom_buffer *b, long long *out, long long off) {
+void nom_buffer_afterbit(struct NomBuffer *b, long long *out, long long off) {
 	if (off < 0) {
 		*out = b->bcap - b->boff - 1;
 
@@ -148,24 +148,24 @@ void nom_buffer_afterbit(struct nom_buffer *b, long long *out, long long off) {
 	}
 }
 
-void nom_buffer_alignbit(struct nom_buffer *b) {
+void nom_buffer_alignbit(struct NomBuffer *b) {
 	b->boff = b->off * 8;
 }
 
 /* bytebuffer functions */
 
-void nom_buffer_writebytes(struct nom_buffer *b, long long off,
+void nom_buffer_writebytes(struct NomBuffer *b, long long off,
 			   long long data_length, unsigned char *data) {
 	memcpy(b->buf + off, data, data_length * sizeof(unsigned char));
 }
 
-void nom_buffer_writebytesnext(struct nom_buffer *b, long long data_length,
+void nom_buffer_writebytesnext(struct NomBuffer *b, long long data_length,
 			       unsigned char *data) {
 	nom_buffer_writebytes(b, b->off, data_length, data);
 	nom_buffer_seekbyte(b, data_length, 1);
 }
 
-void nom_buffer_writeu16le(struct nom_buffer *b, long long off,
+void nom_buffer_writeu16le(struct NomBuffer *b, long long off,
 			   long long data_length, unsigned *data) {
 	for (int i = 0; i < data_length; i++) {
 		b->buf[off + (i * 2)] = (unsigned char)(data[i]);
@@ -173,13 +173,13 @@ void nom_buffer_writeu16le(struct nom_buffer *b, long long off,
 	}
 }
 
-void nom_buffer_writeu16lenext(struct nom_buffer *b, long long data_length,
+void nom_buffer_writeu16lenext(struct NomBuffer *b, long long data_length,
 			       unsigned *data) {
 	nom_buffer_writeu16le(b, b->off, data_length, data);
 	nom_buffer_seekbyte(b, data_length * 2, 1);
 }
 
-void nom_buffer_writeu16be(struct nom_buffer *b, long long off,
+void nom_buffer_writeu16be(struct NomBuffer *b, long long off,
 			   long long data_length, unsigned *data) {
 	for (int i = 0; i < data_length; i++) {
 		b->buf[off + (i * 2)] = (unsigned char)(data[i] >> 8);
@@ -187,13 +187,13 @@ void nom_buffer_writeu16be(struct nom_buffer *b, long long off,
 	}
 }
 
-void nom_buffer_writeu16benext(struct nom_buffer *b, long long data_length,
+void nom_buffer_writeu16benext(struct NomBuffer *b, long long data_length,
 			       unsigned *data) {
 	nom_buffer_writeu16be(b, b->off, data_length, data);
 	nom_buffer_seekbyte(b, data_length * 2, 1);
 }
 
-void nom_buffer_writeu32le(struct nom_buffer *b, long long off,
+void nom_buffer_writeu32le(struct NomBuffer *b, long long off,
 			   long long data_length, unsigned long *data) {
 	for (int i = 0; i < data_length; i++) {
 		b->buf[off + (i * 4)] = (unsigned char)(data[i]);
@@ -203,13 +203,13 @@ void nom_buffer_writeu32le(struct nom_buffer *b, long long off,
 	}
 }
 
-void nom_bufferwriteu32lenext(struct nom_buffer *b, long long data_length,
+void nom_bufferwriteu32lenext(struct NomBuffer *b, long long data_length,
 			      unsigned long *data) {
 	nom_buffer_writeu32le(b, b->off, data_length, data);
 	nom_buffer_seekbyte(b, data_length * 4, 1);
 }
 
-void nom_buffer_writeu32be(struct nom_buffer *b, long long off,
+void nom_buffer_writeu32be(struct NomBuffer *b, long long off,
 			   long long data_length, unsigned long *data) {
 	for (int i = 0; i < data_length; i++) {
 		b->buf[off + (i * 4)] = (unsigned char)(data[i] >> 24);
@@ -219,13 +219,13 @@ void nom_buffer_writeu32be(struct nom_buffer *b, long long off,
 	}
 }
 
-void nom_buffer_writeu32benext(struct nom_buffer *b, long long data_length,
+void nom_buffer_writeu32benext(struct NomBuffer *b, long long data_length,
 			       unsigned long *data) {
 	nom_buffer_writeu32be(b, b->off, data_length, data);
 	nom_buffer_seekbyte(b, data_length * 4, 1);
 }
 
-void nom_buffer_writeu64le(struct nom_buffer *b, long long off,
+void nom_buffer_writeu64le(struct NomBuffer *b, long long off,
 			   long long data_length, unsigned long long *data) {
 	for (int i = 0; i < data_length; i++) {
 		b->buf[off + (i * 8)] = (unsigned char)(data[i]);
@@ -239,13 +239,13 @@ void nom_buffer_writeu64le(struct nom_buffer *b, long long off,
 	}
 }
 
-void nom_buffer_writeu64lenext(struct nom_buffer *b, long long data_length,
+void nom_buffer_writeu64lenext(struct NomBuffer *b, long long data_length,
 			       unsigned long long *data) {
 	nom_buffer_writeu64le(b, b->off, data_length, data);
 	nom_buffer_seekbyte(b, data_length * 8, 1);
 }
 
-void nom_buffer_writeu64be(struct nom_buffer *b, long long off,
+void nom_buffer_writeu64be(struct NomBuffer *b, long long off,
 			   long long data_length, unsigned long long *data) {
 	for (int i = 0; i < data_length; i++) {
 		b->buf[off + (i * 8)] = (unsigned char)(data[i] >> 56);
@@ -259,24 +259,136 @@ void nom_buffer_writeu64be(struct nom_buffer *b, long long off,
 	}
 }
 
-void nom_buffer_writeu64benext(struct nom_buffer *b, long long data_length,
+void nom_buffer_writeu64benext(struct NomBuffer *b, long long data_length,
 			       unsigned long long *data) {
 	nom_buffer_writeu64be(b, b->off, data_length, data);
 	nom_buffer_seekbyte(b, data_length * 8, 1);
 }
 
-void nom_buffer_readbytes(struct nom_buffer *b, unsigned char *out,
+void nom_buffer_readbytes(struct NomBuffer *b, unsigned char *out,
 			  long long off, long long n) {
 	memcpy(out, b->buf + off, n * sizeof(unsigned char));
 }
 
-void nom_buffer_readbytesnext(struct nom_buffer *b, unsigned char *out,
+void nom_buffer_readbytesnext(struct NomBuffer *b, unsigned char *out,
 			      long long n) {
 	nom_buffer_readbytes(b, out, b->off, n);
 	nom_buffer_seekbyte(b, n, 1);
 }
 
-void nom_buffer_seekbyte(struct nom_buffer *b, long long off,
+void nom_buffer_readu16le(struct NomBuffer *b, unsigned *out, long long off,
+			  long long n) {
+	for (int i = 0; i < n; i++) {
+		out[i] = (unsigned)(b->buf[off + (i * 2)]) |
+			 (unsigned)(b->buf[off + (1 + (i * 2))]) << 8;
+	}
+}
+
+void nom_buffer_readu16lenext(struct NomBuffer *b, unsigned *out,
+			      long long n) {
+	nom_buffer_readu16le(b, out, b->off, n);
+	nom_buffer_seekbyte(b, n * 2, 1);
+}
+
+void nom_buffer_readu16be(struct NomBuffer *b, unsigned *out, long long off,
+			  long long n) {
+	for (int i = 0; i < n; i++) {
+		out[i] = (unsigned)(b->buf[off + (1 + (i * 2))]) |
+			 (unsigned)(b->buf[off + (i * 2)]) << 8;
+	}
+}
+
+void nom_buffer_readu16benext(struct NomBuffer *b, unsigned *out,
+			      long long n) {
+	nom_buffer_readu16be(b, out, b->off, n);
+	nom_buffer_seekbyte(b, n * 2, 1);
+}
+
+void nom_buffer_readu32le(struct NomBuffer *b, unsigned long *out,
+			  long long off, long long n) {
+	for (int i = 0; i < n; i++) {
+		out[i] = (unsigned long)(b->buf[off + (i * 4)]) |
+			 (unsigned long)(b->buf[off + (1 + (i * 4))]) << 8 |
+			 (unsigned long)(b->buf[off + (2 + (i * 4))]) << 16 |
+			 (unsigned long)(b->buf[off + (3 + (i * 4))]) << 24;
+	}
+}
+
+void nom_buffer_readu32lenext(struct NomBuffer *b, unsigned long *out,
+			      long long n) {
+	nom_buffer_readu32le(b, out, b->off, n);
+	nom_buffer_seekbyte(b, n * 4, 1);
+}
+
+void nom_buffer_readu32be(struct NomBuffer *b, unsigned long *out,
+			  long long off, long long n) {
+	for (int i = 0; i < n; i++) {
+		out[i] = (unsigned long)(b->buf[off + (3 + (i * 4))]) |
+			 (unsigned long)(b->buf[off + (2 + (i * 4))]) << 8 |
+			 (unsigned long)(b->buf[off + (1 + (i * 4))]) << 16 |
+			 (unsigned long)(b->buf[off + (i * 4)]) << 24;
+	}
+}
+
+void nom_buffer_readu32benext(struct NomBuffer *b, unsigned long *out,
+			      long long n) {
+	nom_buffer_readu32le(b, out, b->off, n);
+	nom_buffer_seekbyte(b, n * 4, 1);
+}
+
+void nom_buffer_readu64le(struct NomBuffer *b, unsigned long long *out,
+			  long long off, long long n) {
+	for (int i = 0; i < n; i++) {
+		out[i] =
+			(unsigned long long)(b->buf[off + (i * 8)]) |
+			(unsigned long long)(b->buf[off + (1 + (i * 8))]) << 8 |
+			(unsigned long long)(b->buf[off + (2 + (i * 8))])
+				<< 16 |
+			(unsigned long long)(b->buf[off + (3 + (i * 8))])
+				<< 24 |
+			(unsigned long long)(b->buf[off + (4 + (i * 8))])
+				<< 32 |
+			(unsigned long long)(b->buf[off + (5 + (i * 8))])
+				<< 40 |
+			(unsigned long long)(b->buf[off + (6 + (i * 8))])
+				<< 48 |
+			(unsigned long long)(b->buf[off + (7 + (i * 8))]) << 56;
+	}
+}
+
+void nom_buffer_readu64lenext(struct NomBuffer *b, unsigned long long *out,
+			      long long n) {
+	nom_buffer_readu64le(b, out, b->off, n);
+	nom_buffer_seekbyte(b, n * 8, 1);
+}
+
+void nom_buffer_readu64be(struct NomBuffer *b, unsigned long long *out,
+			  long long off, long long n) {
+	for (int i = 0; i < n; i++) {
+		out[i] = (unsigned long long)(b->buf[off + (7 + (i * 8))]) |
+			 (unsigned long long)(b->buf[off + (6 + (i * 8))])
+				 << 8 |
+			 (unsigned long long)(b->buf[off + (5 + (i * 8))])
+				 << 16 |
+			 (unsigned long long)(b->buf[off + (4 + (i * 8))])
+				 << 24 |
+			 (unsigned long long)(b->buf[off + (3 + (i * 8))])
+				 << 32 |
+			 (unsigned long long)(b->buf[off + (2 + (i * 8))])
+				 << 40 |
+			 (unsigned long long)(b->buf[off + (1 + (i * 8))])
+				 << 48 |
+			 (unsigned long long)(b->buf[off + (i * 8)]) << 54;
+	}
+}
+
+void nom_buffer_readu64benext(struct NomBuffer *b, unsigned long long *out,
+			      long long n) {
+	nom_buffer_readu64be(b, out, b->off, n);
+	nom_buffer_seekbyte(b, n * 8, 1);
+}
+
+void nom_buffer_seekbyte(struct NomBuffer *b, long long off,
 			 unsigned char relative) {
 	if (relative < 0) {
 		b->off = off;
@@ -286,7 +398,7 @@ void nom_buffer_seekbyte(struct nom_buffer *b, long long off,
 	}
 }
 
-void nom_buffer_afterbyte(struct nom_buffer *b, long long *out, long long off) {
+void nom_buffer_afterbyte(struct NomBuffer *b, long long *out, long long off) {
 	if (off < 0) {
 		*out = b->cap - b->off - 1;
 
@@ -295,13 +407,13 @@ void nom_buffer_afterbyte(struct nom_buffer *b, long long *out, long long off) {
 	}
 }
 
-void nom_buffer_alignbyte(struct nom_buffer *b) {
+void nom_buffer_alignbyte(struct NomBuffer *b) {
 	b->off = b->boff / 8;
 }
 
 /* generic functions */
 
-void nom_buffer_grow(struct nom_buffer *b, long long n) {
+void nom_buffer_grow(struct NomBuffer *b, long long n) {
 	unsigned char *old = b->buf;
 
 	b->buf = malloc(sizeof(*old) + n);
--- nom.h
+++ nom.h
@@ -26,100 +26,188 @@ along with this program.  if not, see <https://www.gnu.org/licenses/>.
 #include <stdio.h>
 
 /* a high-performance buffer type */
-struct nom_buffer {
+typedef struct NomBuffer {
 	/* you shouldn't modify any of these directly, but you *can* */
 	unsigned char *buf;
 	long long off;
 	long long cap;
 	long long boff;
 	long long bcap;
-};
+} NomBuffer;
 
 /* nom_buffer_new creates a new buffer */
-void nom_buffer_new(struct nom_buffer *, long long);
+void nom_buffer_new(struct NomBuffer *, long long);
 
 /* nom_buffer_destroy destroys an existing buffer */
-void nom_buffer_destroy(struct nom_buffer *);
+void nom_buffer_destroy(struct NomBuffer *);
 
 /* nom_buffer_readbit reads a bit from the buffer at the specified offset */
-void nom_buffer_readbit(struct nom_buffer *, unsigned char *, long long);
+void nom_buffer_readbit(struct NomBuffer *, unsigned char *, long long);
 
 /* nom_buffer_readbitnext reads the next bit from the buffer at the current offset and moves the offset forward a bit */
-void nom_buffer_readbitnext(struct nom_buffer *, unsigned char *);
+void nom_buffer_readbitnext(struct NomBuffer *, unsigned char *);
 
 /* nom_buffer_readbits reads n bits from the buffer at the specified offset */
-void nom_buffer_readbits(struct nom_buffer *, unsigned long long *, long long,
+void nom_buffer_readbits(struct NomBuffer *, unsigned long long *, long long,
 			 long long);
 
 /* nom_buffer_readbitsnext reads the next n bits from the buffer at the current offset and moves the offset forward the amount of bits read */
-void nom_buffer_readbitsnext(struct nom_buffer *, unsigned long long *,
+void nom_buffer_readbitsnext(struct NomBuffer *, unsigned long long *,
 			     long long);
 
 /* nom_buffer_setbit sets the bit located at the specified offset */
-void nom_buffer_setbit(struct nom_buffer *, long long);
+void nom_buffer_setbit(struct NomBuffer *, long long);
 
 /* nom_buffer_setbitnext sets the next bit from the current offset and moves the offset forward a bit */
-void nom_buffer_setbitnext(struct nom_buffer *);
+void nom_buffer_setbitnext(struct NomBuffer *);
 
 /* nom_buffer_clearbit clears the bit located at the specified offset */
-void nom_buffer_clearbit(struct nom_buffer *, long long);
+void nom_buffer_clearbit(struct NomBuffer *, long long);
 
 /* nom_buffer_clearbitnext clears the next bit from the current offset and moves the offset forward a bit */
-void nom_buffer_clearbitnext(struct nom_buffer *);
+void nom_buffer_clearbitnext(struct NomBuffer *);
 
 /* nom_buffer_setbits sets the next n bits from the specified offset value */
-void nom_buffer_setbits(struct nom_buffer *, long long, unsigned long long,
+void nom_buffer_setbits(struct NomBuffer *, long long, unsigned long long,
 			long long);
 
 /* nom_buffer_setbitsnext sets the next n bits from the current offset and moves the offset forward the amount of bits set */
-void nom_buffer_setbitsnext(struct nom_buffer *, unsigned long long, long long);
+void nom_buffer_setbitsnext(struct NomBuffer *, unsigned long long, long long);
 
 /* nom_buffer_flipbit flips the bit located at the specified offset */
-void nom_buffer_flipbit(struct nom_buffer *, long long);
+void nom_buffer_flipbit(struct NomBuffer *, long long);
 
 /* nom_buffer_flipbitnext flips the bit located at the current offset and moves the offset forwards a bit */
-void nom_buffer_flipbitnext(struct nom_buffer *);
+void nom_buffer_flipbitnext(struct NomBuffer *);
 
 /* nom_buffer_clearallbits clears all of the bits in the buffer */
-void nom_buffer_clearallbits(struct nom_buffer *);
+void nom_buffer_clearallbits(struct NomBuffer *);
 
 /* nom_buffer_setallbits sets all of the bits in the buffer */
-void nom_buffer_setallbits(struct nom_buffer *);
+void nom_buffer_setallbits(struct NomBuffer *);
 
 /* nom_buffer_flipallbits flips all of the bits in the buffer */
-void nom_buffer_flipallbits(struct nom_buffer *);
+void nom_buffer_flipallbits(struct NomBuffer *);
 
 /* nom_buffer_seekbit seeks to bit position off of buffer relative to the current position or exact */
-void nom_buffer_seekbit(struct nom_buffer *, long long, unsigned char);
+void nom_buffer_seekbit(struct NomBuffer *, long long, unsigned char);
 
 /* nom_buffer_afterbit stores the amount of bits located after the current position or the specified one in out */
-void nom_buffer_afterbit(struct nom_buffer *, long long *, long long);
+void nom_buffer_afterbit(struct NomBuffer *, long long *, long long);
 
 /* nom_buffer_alignbit aligns the bit offset to the byte offset */
-void nom_buffer_alignbit(struct nom_buffer *);
+void nom_buffer_alignbit(struct NomBuffer *);
 
 /* nom_buffer_writebytes writes a byte array to the buffer at the specified offset */
-void nom_buffer_writebytes(struct nom_buffer *, long long, long long,
+void nom_buffer_writebytes(struct NomBuffer *, long long, long long,
 			   unsigned char *);
 
 /* nom_buffer_writebytesnext writes a byte array to the buffer at the current offset and moves the offset forward the amount of bytes written */
-void nom_buffer_writebytesnext(struct nom_buffer *, long long, unsigned char *);
+void nom_buffer_writebytesnext(struct NomBuffer *, long long, unsigned char *);
+
+/* nom_buffer_writeu16le writes an array of u16s to the buffer in little endian at the specified offset */
+void nom_buffer_writeu16le(struct NomBuffer *, long long, long long,
+			   unsigned *);
+
+/* nom_buffer_writeu16lenext writes an array of u16s to the buffer in little endian at the current offset and moves the offset forward the amount of bytes written */
+void nom_buffer_writeu16lenext(struct NomBuffer *, long long, unsigned *);
+
+/* nom_buffer_writeu16be writes an array of u16s to the buffer in big endian at the specified offset */
+void nom_buffer_writeu16be(struct NomBuffer *, long long, long long,
+			   unsigned *);
+
+/* nom_buffer_writeu16lenext writes an array of u16s to the buffer in big endian at the current offset and moves the offset forward the amount of bytes written */
+void nom_buffer_writeu16benext(struct NomBuffer *, long long, unsigned *);
+
+/* nom_buffer_writeu32le writes an array of u32s to the buffer in little endian at the specified offset */
+void nom_buffer_writeu32le(struct NomBuffer *, long long, long long,
+			   unsigned long *);
+
+/* nom_buffer_writeu32lenext writes an array of u32s to the buffer in little endian at the current offset and moves the offset forward the amount of bytes written */
+void nom_buffer_writeu32lenext(struct NomBuffer *, long long, unsigned long *);
+
+/* nom_buffer_writeu32be writes an array of u32s to the buffer in big endian at the specified offset */
+void nom_buffer_writeu32be(struct NomBuffer *, long long, long long,
+			   unsigned long *);
+
+/* nom_buffer_writeu32benext writes an array of u32s to the buffer in big endian at the current offset and moves the offset forward the amount of bytes written */
+void nom_buffer_writeu32benext(struct NomBuffer *, long long, unsigned long *);
+
+/* nom_buffer_writeu64le writes an array of u64s to the buffer in little endian at the specified offset */
+void nom_buffer_writeu64le(struct NomBuffer *, long long, long long,
+			   unsigned long long *);
+
+/* nom_buffer_writeu64lenext writes an array of u64s to the buffer in little endian at the current offset and moves the offset forward the amount of bytes written */
+void nom_buffer_writeu64lenext(struct NomBuffer *, long long,
+			       unsigned long long *);
+
+/* nom_buffer_writeu64be writes an array of u64s to the buffer in big endian at the specified offset */
+void nom_buffer_writeu64be(struct NomBuffer *, long long, long long,
+			   unsigned long long *);
+
+/* nom_buffer_writeu64benext writes an array of u64s to the buffer in big endian at the current offset and moves the offset forward the amount of bytes written */
+void nom_buffer_writeu64benext(struct NomBuffer *, long long,
+			       unsigned long long *);
 
 /* nom_buffer_readbytes reads n bytes from the buffer at the specified offset */
-void nom_buffer_readbytes(struct nom_buffer *, unsigned char *, long long,
+void nom_buffer_readbytes(struct NomBuffer *, unsigned char *, long long,
 			  long long);
 
 /* nom_buffer_readbytesnext reads n bytes from the buffer at the current offset and moves the offset forward the amount of bytes read */
-void nom_buffer_readbytesnext(struct nom_buffer *, unsigned char *, long long);
+void nom_buffer_readbytesnext(struct NomBuffer *, unsigned char *, long long);
+
+/* nom_buffer_readu16le reads n u16s from the buffer in little endian at the specified offset */
+void nom_buffer_readu16le(struct NomBuffer *, unsigned *, long long,
+			  long long);
+
+/* nom_buffer_readu16lenext reads n u16s from the buffer in little endian at the current offset and moves the offset forward the amount of bytes read */
+void nom_buffer_readu16lenext(struct NomBuffer *, unsigned *, long long);
+
+/* nom_buffer_readu16be reads n u16s from the buffer in big endian at the specified offset */
+void nom_buffer_readu16be(struct NomBuffer *, unsigned *, long long,
+			  long long);
+
+/* nom_buffer_readu16benext reads n u16s from the buffer in big endian at the current offset and moves the offset forward the amount of bytes read */
+void nom_buffer_readu16benext(struct NomBuffer *, unsigned *, long long);
+
+/* nom_buffer_readu32le reads n u32s from the buffer in little endian at the specified offset */
+void nom_buffer_readu32le(struct NomBuffer *, unsigned long *, long long,
+			  long long);
+
+/* nom_buffer_readu32lenext reads n u32s from the buffer in little endian at the current offset and moves the offset forward the amount of bytes read */
+void nom_buffer_readu32lenext(struct NomBuffer *, unsigned long *, long long);
+
+/* nom_buffer_readu32be reads n u32s from the buffer in big endian at the specified offset */
+void nom_buffer_readu32be(struct NomBuffer *, unsigned long *, long long,
+			  long long);
+
+/* nom_buffer_readu32benext reads n u32s from the buffer in big endian at the current offset and moves the offset forward the amount of bytes read */
+void nom_buffer_readu32benext(struct NomBuffer *, unsigned long *, long long);
+
+/* nom_buffer_readu64le reads n u64s from the buffer in little endian at the specified offset */
+void nom_buffer_readu64le(struct NomBuffer *, unsigned long long *, long long,
+			  long long);
+
+/* nom_buffer_readu64lenext reads n u64s from the buffer in little endian at the current offset and moves the offset forward the amount of bytes read */
+void nom_buffer_readu64lenext(struct NomBuffer *, unsigned long long *,
+			      long long);
+
+/* nom_buffer_readu64be reads n u64s from the buffer in big endian at the specified offset */
+void nom_buffer_readu64be(struct NomBuffer *, unsigned long long *, long long,
+			  long long);
+
+/* nom_buffer_readu64benext reads n u64s from the buffer in big endian at the current offset and moves the offset forward the amount of bytes read */
+void nom_buffer_readu64benext(struct NomBuffer *, unsigned long long *,
+			      long long);
 
 /* nom_buffer_seekbytes seeks to position off of the buffer relative to the current position or exact */
-void nom_buffer_seekbyte(struct nom_buffer *, long long, unsigned char);
+void nom_buffer_seekbyte(struct NomBuffer *, long long, unsigned char);
 
 /* nom_buffer_afterbyte stores the amount of bytes located after the current position or the specified one in out */
-void nom_buffer_afterbyte(struct nom_buffer *, long long *, long long);
+void nom_buffer_afterbyte(struct NomBuffer *, long long *, long long);
 
 /* nom_buffer_alignbyte aligns the byte offset to the bit offset */
-void nom_buffer_alignbyte(struct nom_buffer *);
+void nom_buffer_alignbyte(struct NomBuffer *);
 
 /* nom_buffer_grow makes the buffer's capacity bigger by n bytes */
-void nom_buffer_grow(struct nom_buffer *, long long);
+void nom_buffer_grow(struct NomBuffer *, long long);
