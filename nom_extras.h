50f20bdeb561cd62afa22c176cde4e133839d3c4
superwhiskers <whiskerdev@protonmail.com>
2023-01-13 16:57:55 -0600

    nom: make the license notice verbatim

--- nom_extras.h
+++ nom_extras.h
@@ -3,9 +3,9 @@
 nom - crunch but the letter c
 copyright (c) 2019 superwhiskers <whiskerdev@protonmail.com>
 
-this source code form is subject to the terms of the mozilla public
-license, v. 2.0. if a copy of the mpl was not distributed with this
-file, you can obtain one at http://mozilla.org/MPL/2.0/.
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 */
 
